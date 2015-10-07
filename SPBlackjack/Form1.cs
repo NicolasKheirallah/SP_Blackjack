85034b837ddebd2fb13f7cd2d612a81693a385eb
Nicolas Kheirallah <nicolas.kheirallah@outlook.com>
2015-10-07 14:31:19 +0200

    updated

--- SPBlackjack/Form1.cs
+++ SPBlackjack/Form1.cs
@@ -11,31 +11,52 @@ namespace BlackJack
         Deck currentDeck = new Deck();
         List<Card> player = new List<Card>();
         List<Card> dealer = new List<Card>();
-        int playerSum ,dealerSum, playerDealtCard, dealerDealtCard, playerScore,dealerScore;
-        bool dealerTurn;
+        int playerValue ,dealerValue, playerDealtCard, dealerDealtCard, playerScore,dealerScore;
+        bool dealerTurn, bustedPlayer,bustedDealer;
         
         public blackJackForm()
         {
             InitializeComponent();
-         //   label2.Text = Convert.ToString(playerScore); // didnt work and even with refresh...
-         //   label3.Text = Convert.ToString(dealerScore);
+            //   label2.Text = Convert.ToString(playerScore); // didnt work and even with refresh...
+            //   label3.Text = Convert.ToString(dealerScore);
+            currentDeck.shuffle(); // shuffles the deck
             hitBtn.Enabled = false;
             stayBtn.Enabled = false;
         }
+        /// <summary>
+        /// calculate the size of the players hand , checks that the hand isnt to big
+        /// </summary>
+        /// <param name="playerCards"></param>
         private void calculatePlayerHand(Card playerCards)
         {
-                    playerSum += Convert.ToInt32(playerCards.Value);
+            playerValue += Convert.ToInt32(playerCards.Value);
+            if (isBusted(playerValue))
+            { 
+                bustedPlayer = true;
+            }
         }
-
+        /// <summary>
+        /// calcualtes the value and checks that it doesnt go over 21
+        /// </summary>
+        /// <param name="dealerCards"></param>
         private void calculateDealerHand(Card dealerCards)
         {
-                    dealerSum += Convert.ToInt32(dealerCards.Value);
+            dealerValue += Convert.ToInt32(dealerCards.Value);
+            if(isBusted(dealerValue))
+            {
+                bustedDealer = true;
+            }
         }
-
+        /// <summary>
+        /// Checks that the value isnt over 21
+        /// </summary>
+        /// <param name="sum"></param>
+        /// <returns></returns>
         private bool isBusted(int sum)
         {
             if (sum > 21) // if card sum is bigger than 21 , then the person loses
             {
+
                 return true;
             }
             else
@@ -43,10 +64,13 @@ namespace BlackJack
                 return false;
             }
         }
-
+        /// <summary>
+        /// Deal button , deals two hands and puts them in the listbox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void dealButton_Click(object sender, EventArgs e)
         {
-            currentDeck.shuffle();
             for (int i = 0; i < 2; i++) // adds two cards for each new game
             {
                 dealer.Add(currentDeck.deal());
@@ -64,27 +88,36 @@ namespace BlackJack
             {
                 calculateDealerHand(dealerHand);
             }
- 
-            playerValueLbl.Text = playerSum.ToString();
-            dealerValueLbl.Text = dealerSum.ToString();
+
+            playerValueLbl.Text = playerValue.ToString();
+            dealerValueLbl.Text = dealerValue.ToString();
             dealBtn.Enabled = false;
             hitBtn.Enabled = true;
             stayBtn.Enabled = true;
-                  
+            checkValue();
         }
-        private void clear()
+        /// <summary>
+        /// resets everything
+        /// </summary>
+        private void clear() 
         {
-            dealerLB.Items.Clear();
-            playerLB.Items.Clear();
             dealer.Clear();
             player.Clear();
+            dealerLB.Items.Clear();
+            playerLB.Items.Clear();
             dealerValueLbl.Text = " ";
             playerValueLbl.Text = " ";
-            dealerSum = 0;
-            playerSum = 0;
+            dealerValue = 0;
+            playerValue = 0;
             playerDealtCard = 2;
             dealerDealtCard = 2;
         }
+
+        /// <summary>
+        /// clean , shuffle 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void shuffleButton_Click(object sender, EventArgs e)
         {
             clear();
@@ -92,24 +125,43 @@ namespace BlackJack
             dealBtn.Enabled = true;          
         }
 
-        private void hitButton_Click(object sender, EventArgs e)
+        private void checkValue()
         {
-            
-            player.Add(currentDeck.deal());
-            calculatePlayerHand(player[playerDealtCard]);
-            playerLB.Items.Add(player[playerDealtCard]);
-            playerValueLbl.Text = playerSum.ToString();
-
-            if (isBusted(playerSum))
+            if (isBusted(playerValue)|| bustedPlayer == true)
             {
                 MessageBox.Show("Your Hand is busted, The Dealer Wins!");
+                win(false);
                 hitBtn.Enabled = false;
                 stayBtn.Enabled = false;
                 dealBtn.Enabled = false;
-                win(false);
+                bustedPlayer = false;
             }
+           if (isBusted(dealerValue)||bustedDealer == true)
+            {
+                MessageBox.Show("Dealer hand is busted,You Wins!");
+                win(true);
+                bustedDealer = false;
+                hitBtn.Enabled = false;
+                stayBtn.Enabled = false;
+                dealBtn.Enabled = false;
+            }
+
+
+        }
+
+        private void hitButton_Click(object sender, EventArgs e)
+        {
+            player.Add(currentDeck.deal());
+            calculatePlayerHand(player[playerDealtCard]);
+            playerLB.Items.Add(player[playerDealtCard]);
+            playerValueLbl.Text = playerValue.ToString();
+            checkValue();
             playerDealtCard++;
         }
+        /// <summary>
+        /// Yup winning , increase score with +1 
+        /// </summary>
+        /// <param name="winning"></param>
 
         private void win(bool winning)
         {
@@ -124,6 +176,11 @@ namespace BlackJack
 
             }
         }
+        /// <summary>
+        /// Stay button == No more cards for you
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void stayButton_Click(object sender, EventArgs e)
         {
             hitBtn.Enabled = false;
@@ -131,21 +188,14 @@ namespace BlackJack
             dealerTurn = true;
             while (dealerTurn)
             {
-                if (dealerSum <= 15) // if the dealer has a sum under 15 then they must draw a card. Casino rules
+                if (dealerValue <= 15) // if the dealer has a sum under 15 then they must draw a card. Casino rules
                 {
                     dealer.Add(currentDeck.deal());
                     calculateDealerHand(dealer[dealerDealtCard]);
                     dealerLB.Items.Add(dealer[dealerDealtCard]);
-                    dealerValueLbl.Text = dealerSum.ToString();
+                    dealerValueLbl.Text = dealerValue.ToString();
                     dealerDealtCard++;
-                    if (isBusted(dealerSum))
-                    {
-                        MessageBox.Show("Dealer hand is busted!");
-                        win(true);
-                        hitBtn.Enabled = false;
-                        stayBtn.Enabled = false;
-                        dealBtn.Enabled = false;
-                    }
+                    checkValue();
                 }
                 else
                 {
@@ -153,14 +203,19 @@ namespace BlackJack
                 }
             }
 
-            if (!isBusted(playerSum) && playerSum > dealerSum)
+            if (!isBusted(playerValue) && playerValue > dealerValue)
             {
-                MessageBox.Show("Player Wins!");
+                MessageBox.Show("Player Wins This Round!");
                 win(true);
             }
-            else if (!isBusted(dealerSum) && dealerSum > playerSum)
+            else if (!isBusted(dealerValue) && dealerValue > playerValue)
             {
-                MessageBox.Show("Dealer Wins!");
+                MessageBox.Show("Dealer Wins This Round!");
+                win(false);
+            }
+            else if(!isBusted(dealerValue) && dealerValue  == playerValue)
+            {
+                MessageBox.Show("Dealer Wins This Round!");
                 win(false);
             }
         }
